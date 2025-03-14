de1e74f7ea590754996d9bd27a2c6b6115db6cd6
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-03-14 15:40:04 -0300

    build: create the basic structure of the project

---
+++ src/main/java/dev/java10/ShinobiRegistrationSystem/ShinobiRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.java10.ShinobiRegistrationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinobiRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinobiRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10/ShinobiRegistrationSystem/ShinobiRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10.ShinobiRegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinobiRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
