2ae61914ff974b7c5df6ebe2b8667ea612e87186
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-17 10:59:49 -0300

    feat: add ninja dto in controler and servie

--- src/main/java/dev/java10/ShinobiRegistrationSystem/Ninjas/NinjaService.java
+++ src/main/java/dev/java10/ShinobiRegistrationSystem/Ninjas/NinjaService.java
@@ -8,30 +8,42 @@ import java.util.Optional;
 @Service
 public class NinjaService {
     private final NinjaRepository ninjaRepository;
+    private final NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
-    public NinjaModel createNinja(NinjaModel ninja){
-        return this.ninjaRepository.save(ninja);
+    public NinjaDTO createNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja = this.ninjaMapper.map(ninjaDTO);
+
+        NinjaModel ninjaModel = this.ninjaRepository.save(ninja);
+
+        return this.ninjaMapper.map(ninjaModel);
+
     }
 
-    public List<NinjaModel> getNinjas(){
-        return this.ninjaRepository.findAll();
+    public List<NinjaDTO> getNinjas(){
+        List<NinjaModel> ninjasModel = this.ninjaRepository.findAll();
+
+        return ninjasModel.stream().map(this.ninjaMapper::map).toList();
     }
 
-    public NinjaModel getNinjaById(Long id){
+    public NinjaDTO getNinjaById(Long id){
         Optional<NinjaModel> ninja = this.ninjaRepository.findById(id);
 
-        return ninja.orElse(null);
+        return ninja.map(this.ninjaMapper::map).orElse(null);
+
     }
 
-    public NinjaModel updateNinja(Long id, NinjaModel updatedNinja){
+    public NinjaDTO updateNinja(Long id, NinjaDTO updatedNinja){
         if(this.ninjaRepository.existsById(id)){
             updatedNinja.setId(id);
 
-           return this.ninjaRepository.save(updatedNinja);
+            NinjaModel ninjaModel = this.ninjaMapper.map(updatedNinja);
+
+           return this.ninjaMapper.map(ninjaModel);
         }
 
         return null;
