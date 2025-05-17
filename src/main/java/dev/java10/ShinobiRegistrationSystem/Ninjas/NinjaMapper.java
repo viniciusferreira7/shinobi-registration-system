3e74646aba85d252634b9eeae983ea1e067a6cbe
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-17 10:59:17 -0300

    feat: create ninja mapper to mapping dto and model

--- src/main/java/dev/java10/ShinobiRegistrationSystem/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10/ShinobiRegistrationSystem/Ninjas/NinjaMapper.java
@@ -1,4 +1,31 @@
 package dev.java10.ShinobiRegistrationSystem.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setAge(ninjaDTO.getAge());
+        ninjaModel.setMission(ninjaDTO.getMission());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setAge(ninjaModel.getAge());
+        ninjaDTO.setMission(ninjaModel.getMission());
+
+        return ninjaDTO;
+    }
 }
